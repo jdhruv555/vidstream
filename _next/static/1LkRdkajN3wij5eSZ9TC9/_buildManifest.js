acfbff8eb5785d8e285a9a84a7c77b476c41c7d6
Dhruv <135132021+jdhruv555@users.noreply.github.com>
2024-11-30 14:27:05 +0530

    Updates

---
+++ _next/static/1LkRdkajN3wij5eSZ9TC9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/5e2a4920-6ceab51657345c5f.js","static/chunks/pages/index-326a3d0d93b60e4f.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
